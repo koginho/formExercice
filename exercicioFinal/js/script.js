f4118d48f83e9c1ae54a070dc063949a7117ee32
koginho <mcatalanodraw@gmail.com>
2025-08-04 16:59:46 -0300

    [ADD] Factory criada, e div para resultados foi adicionada.

--- exercicioFinal/js/script.js
+++ exercicioFinal/js/script.js
@@ -1,5 +1,32 @@
 function escopoPrincipal () {
-    const form = document.querySelector('.formIMC');
-};
+    const formIMC = document.querySelector('.formIMC');
+    const resultadoIMC = document.querySelector('.resultadoFormIMC')
+
+    const pessoas = []; // Array para coleta de dados
+
+    let contador = 1 // Variável para contador criada
+
+    function recebeEventoForm (evento) {
+        evento.preventDefault(); // cancela o evento sem parar a programação
+        console.log(`Número de formulários enviados: ${contador}`)
+        contador++; // Adiciona mais '1' para a variável contador
+
+        const nome = formIMC.querySelector('.nome');
+        const sobrenome = formIMC.querySelector('.sobrenome');
+        const peso = formIMC.querySelector('.peso');
+        const altura = formIMC.querySelector('.altura');
+
+        function criaPessoa (nome, sobrenome, peso, altura) { 
+            return {nome, sobrenome, peso, altura}
+        }; //factory
+
+        console.log(nome.value, sobrenome.value, peso.value, altura.value)
+    }
+
+    formIMC.addEventListener('submit', recebeEventoForm) // Adiciona um espião para escutar um evento
+
+    resultadoIMC.innerHTML = `<p>${pessoas}</p>`
+    
+}
 
 escopoPrincipal();
